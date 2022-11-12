f000de2fd5663ff72eb739e91c388450e4987a61
Dylan Cluyse <dylan.cluyse@student.hogent.be>
2022-11-12 17:12:40 +0100

    structuur aanpassen

---
+++ Kaggle/Kaggle_Scrabble/src/main/java/be/hogent/dit/tin/KaggleLinearRegression.java
@@ -0,0 +1,110 @@
+package be.hogent.dit.tin;
+
+import static org.apache.spark.sql.functions.col;
+
+import org.apache.spark.ml.feature.MinMaxScaler;
+import org.apache.spark.ml.feature.MinMaxScalerModel;
+import org.apache.spark.ml.feature.StringIndexer;
+import org.apache.spark.ml.feature.VectorAssembler;
+import org.apache.spark.ml.linalg.Vectors;
+import org.apache.spark.ml.regression.LinearRegression;
+import org.apache.spark.ml.regression.LinearRegressionModel;
+import org.apache.spark.ml.regression.LinearRegressionSummary;
+import org.apache.spark.ml.regression.LinearRegressionTrainingSummary;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+public class KaggleLinearRegression {
+
+	SparkSession spark = SparkSession.builder().appName("KaggleLinearRegression").master("local[*]").getOrCreate();
+
+	private Dataset<Row> getData() {
+		return this.spark.read().option("header", true).csv("src/main/resources/games.csv");
+	}
+
+	private Dataset<Row> dropColumns(Dataset<Row> dataset) {
+		return dataset.drop(col("game_id")).drop(col("first")).drop(col("time_control_name"))
+				.drop(col("game_end_reason")).drop(col("created_at")).drop(col("lexicon")).drop(col("rating_mode"));
+	}
+
+	private Dataset<Row> changeTypeColumns(Dataset<Row> dataset) {
+		return dataset.withColumn("initial_time_seconds", dataset.col("initial_time_seconds").cast("Integer"))
+				.withColumn("increment_seconds", dataset.col("increment_seconds").cast("Integer"))
+				.withColumn("max_overtime_minutes", dataset.col("max_overtime_minutes").cast("Integer"))
+				.withColumn("game_duration_seconds", dataset.col("game_duration_seconds").cast("Integer"));
+	}
+
+	private Dataset<Row> stringIndexing(Dataset<Row> dataset) {
+		StringIndexer indexer = new StringIndexer().setInputCol("winner").setOutputCol("ind_winner");
+
+		return indexer.fit(dataset).transform(dataset);
+	}
+
+	private Dataset<Row> assemblingFeatures(Dataset<Row> dataset) {
+		VectorAssembler assembler = new VectorAssembler().setInputCols(new String[] { "initial_time_seconds",
+				"increment_seconds", "max_overtime_minutes", "game_duration_seconds", "ind_winner" })
+				.setOutputCol("features");
+
+		return assembler.transform(dataset);
+	}
+
+	private Dataset<Row> minmaxScaling(Dataset<Row> dataset) {
+		MinMaxScaler scaler = new MinMaxScaler().setInputCol("features").setOutputCol("scaledFeatures");
+
+		MinMaxScalerModel scalingModel = scaler.fit(dataset);
+
+		return scalingModel.transform(dataset);
+	}
+
+	private Dataset<Row>[] splitSets(Dataset<Row> dataset, double[] proportie) {
+		Dataset<Row> final_data = dataset.select(col("features"), col("game_duration_seconds"));
+		return final_data.randomSplit(proportie);
+	}
+	
+	private LinearRegressionModel getLinearRegModel(Dataset<Row> dataset) {
+		LinearRegression lr = new LinearRegression()
+				.setFeaturesCol("features")
+				.setLabelCol("game_duration_seconds");
+		
+		return lr.fit(dataset);
+	}
+
+	public static void main(String[] args) {
+
+		KaggleLinearRegression linreg = new KaggleLinearRegression();
+
+		Dataset<Row> regression = linreg.getData();
+
+		regression = linreg.dropColumns(regression);
+		regression = linreg.changeTypeColumns(regression);
+		regression = linreg.stringIndexing(regression);
+		regression = regression.drop(col("winner"));
+		regression = linreg.assemblingFeatures(regression);
+		// regression = linreg.minmaxScaling(regression);
+
+		double[] proportie = { 0.8, 0.2 };
+		Dataset<Row>[] sets = linreg.splitSets(regression, proportie);
+		
+		LinearRegressionModel model = linreg.getLinearRegModel(sets[0]);
+
+		LinearRegressionSummary testSummary = model.evaluate(sets[1]);
+
+		System.out.println("Coefficients: " + model.coefficients() + " Intercept: " + model.intercept());
+
+		LinearRegressionTrainingSummary trainingSummary = model.summary();
+		
+		//System.out.println("numIterations: " + trainingSummary.totalIterations());
+		//System.out.println("objectiveHistory: " + Vectors.dense(trainingSummary.objectiveHistory()));
+
+		trainingSummary.residuals().show();
+
+		System.out.println("Trainingset --> RMSE: " + trainingSummary.rootMeanSquaredError());
+		System.out.println("Testset --> RMSE: " + testSummary.rootMeanSquaredError());
+
+		System.out.println("Trainingset --> r2: " + trainingSummary.r2());
+		System.out.println("Testset --> r2: " + testSummary.r2());
+
+	}
+
+}
---
+++ Kaggle/Kaggle_Scrabble/src/main/java/be/hogent/dit/tin/KaggleLogisticRegression.java
@@ -0,0 +1,10 @@
+package be.hogent.dit.tin;
+
+public class KaggleLogisticRegression {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
